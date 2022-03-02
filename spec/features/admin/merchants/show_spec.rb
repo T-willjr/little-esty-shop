0b042bc27ee783957fc85417c38b71143f7d5871
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-01 20:10:38 -0500

    Test: Merchant name link on index page takes user to merchant show page

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.describe "Admin Merchant Show Page" do
   it "displays merchant name " do
     merchant = Merchant.create!(name: 'Chuckin Pucks')
-    visit "/admin/merchants/#{merchant.id}"
+    merchant2 = Merchant.create!(name: 'Daisys Daffodils')
+
+    visit "/admin/merchants"
+    click_on(merchant.name)
+
+    expect(current_path).to eq("/admin/merchants/#{merchant.id}")
     expect(page).to have_content(merchant.name)
+    expect(page).to_not have_content(merchant2.name)
   end
 
   it "has form to updates merchant name" do
